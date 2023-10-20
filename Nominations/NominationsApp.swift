bf8e0703c64cacacc0147d41ce95d1686d8a46cf
Sam Davis <samdavis@3sidedcube.com>
2023-10-20 11:41:37 +0100

    Initial Commit

---
+++ Nominations/NominationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NominationsApp.swift
+//  Nominations
+//
+//  Created by Sam Davis on 20/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct NominationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
